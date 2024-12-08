1a81532183e2d50f26752a0986f9fc195a9552d2
Kiyoos <kiyoos@byui.edu>
2024-12-07 18:06:53 -0800

    Fixing loading activities

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,9 @@
-import { fetchActivities, highlight, activityList } from './activities';
+import { fetchActivities, activityList } from './activities';
 import calendar from './calendar';
 import { loadHeaderFooter, serverDeploy } from './utils.mjs';
-import fetchWeather from './weather';
 
 loadHeaderFooter();
 calendar();
 activityList();
+fetchActivities();
 // serverDeploy();
